91f72a2b9341deed8fc259bbf58c02ec22c22ef0
Alessandro Sieni <Alexander.sieni@gmail.com>
2019-03-20 19:50:29 +0100

    aggiunto anche esempio con parametri e ritorno intero

--- src/Java/example.java
+++ src/Java/example.java
@@ -9,6 +9,7 @@ public class example {
         interp.execfile("../python/examplescript.py");
         PyFunction fun = (PyFunction)interp.get("fun", PyFunction.class);
         PyFunction fun2 = (PyFunction)interp.get("fun2", PyFunction.class);
+        PyFunction prod = (PyFunction)interp.get("product", PyFunction.class);
 
         String ret = ((PyString)fun.__call__()).asString();
 
@@ -16,6 +17,9 @@ public class example {
         System.out.println(ret);
         ret = ((PyString)fun2.__call__()).asString();
         System.out.println(ret);
+        PyInteger _p = (PyInteger)(prod.__call__(new PyInteger(3),new PyInteger(5)));
+        int p = (int)(_p.getValue());
+        System.out.println(p);
     }
 
 }
