673128cebafb8c9e45096bd9291174fc6fdd7afe
Alessandro Sieni <Alexander.sieni@gmail.com>
2019-05-08 17:36:34 +0200

    Aggiunta della funzione che calcola la distanza dai centri

--- src/Python/FCM.py
+++ src/Python/FCM.py
@@ -2,8 +2,14 @@ import pandas as pd
 import skfuzzy as fuzz
 import numpy as np
 import json
+from scipy.spatial.distance import cdist
+import os
 ERROR_THRESHOLD = 0.005
+DISTANCE_THRESHOLD = 4
+VALUES_THRESHOLD = 2
 MAX_ITER = 1000
+NEW_VALUES = -5
+MODEL_PATH = "../data/newModel.json"
 class FCM:
     def merge(self):
         return "Models merged",200
@@ -12,9 +18,8 @@ class FCM:
         df = pd.read_csv("../data/readyData.txt",names=["X","Y"],header=None,dtype={"X":float,"Y":float})
         #Generating the numpy array from the dataframe in order to use it in the FCM
         values = np.array(df.values)
-        print("-----------------------------------------------------------------")
-        print(values)
-        print("-----------------------------------------------------------------")
+        #Checking if the model must be computed
+        new_values = values[NEW_VALUES:]
         #Training the FCM with the array just obtained
         cntr,u_orig, _, _, _, _, _ = fuzz.cluster.cmeans(values.T,2,2,error=ERROR_THRESHOLD,maxiter = MAX_ITER)
         #Creating the JSON with the information of the created model
@@ -22,7 +27,23 @@ class FCM:
         model["centers"] = cntr.tolist()
         model["coefficentsMatrix"] = u_orig.tolist()
         #Saving the JSON in the file
-        with open("../data/newModel.json","w") as newModelFile:
+        with open(MODEL_PATH,"w") as newModelFile:
             newModelFile.write(json.dumps(model))
         #Returning the OK code
         return "Model created",200
+    def isModelNeeded(self,values):
+        fileExists = os.path.isfile(MODEL_PATH)
+        if fileExists:
+            with open(MODEL_PATH,"r") as modelFile:
+                centers = np.array(json.load(modelFile)["centers"])
+                minDistances = np.amin(cdist(values,centers,metric='euclidean'),axis=1)
+                print(minDistances)
+                print(np.count_nonzero(minDistances[minDistances > DISTANCE_THRESHOLD]))
+            return False
+        else:
+            return True
+
+
+if __name__ == "__main__":
+    print("Prova train")
+    FCM().isModelNeeded(np.array([[2,3],[1,2],[2,4]]))
