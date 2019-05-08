0db6339da405432b48a4f75e3bbbc125fd7c7037
Alessandro Sieni <Alexander.sieni@gmail.com>
2019-05-08 09:17:00 +0200

    Rest Server Aggiornato e corretto

--- src/Python/FCM.py
+++ src/Python/FCM.py
@@ -1,24 +1,25 @@
 import pandas as pd
 import skfuzzy as fuzz
 import numpy as np
+import json
 ERROR_THRESHOLD = 0.005
 MAX_ITER = 1000
 class FCM:
     def update():
         return "Model updated",200
-    def train():
+    def train(self):
         #Retriving the dataframe related to the generated file
-        df = pd.read_csv("../data/collectedDate.txt",names=["X","Y"],header=None,dtype={"X":float,"Y":float})
+        df = pd.read_csv("../data/collectedData.txt",names=["X","Y"],header=None,dtype={"X":float,"Y":float})
         #Generating the numpy array from the dataframe in order to use it in the FCM
         values = np.array(df.values)
         #Training the FCM with the array just obtained
-        cntr,u_orig, _, _, _, _, _ = fuzz.cluster.cmeans(array.T,2,2,error=ERROR_THRESHOLD,maxiter = 1000)
+        cntr,u_orig, _, _, _, _, _ = fuzz.cluster.cmeans(values.T,2,2,error=ERROR_THRESHOLD,maxiter = 1000)
         #Creating the JSON with the information of the created model
         model = {}
-        model["centers"] = cntr
-        model["coefficentsMatrix"] = u_orig
+        model["centers"] = cntr.tolist()
+        model["coefficentsMatrix"] = u_orig.tolist()
         #Saving the JSON in the file
-        with open("../data/newModel.json") as newModelFile:
+        with open("../data/newModel.json","w") as newModelFile:
             newModelFile.write(json.dumps(model))
-        #Returning the OK code 
+        #Returning the OK code
         return "Model created",200
