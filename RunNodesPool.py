2c16b823b4b3bdb8662f1c02544400c3a1d9e292
Alessandro Sieni <Alexander.sieni@gmail.com>
2019-05-16 13:08:16 +0200

    Sistema script lancio nodi

--- RunNodesPool.py
+++ RunNodesPool.py
@@ -6,9 +6,10 @@ from time import sleep
 
 parser = argparse.ArgumentParser(description='Run a pool of nodes for testing purpose')
 parser.add_argument("poolSize", help='number of nodes to run')
+parser.add_argument("ipRabbit",help="IP address of the rabbit mq server")
 args = parser.parse_args()
 
 pidList = []
 
 for node in range(0,int(args.poolSize)):
-    pidList.append(subprocess.Popen(["java", "-jar", "DataCollector.jar"], shell=True))
+    pidList.append(subprocess.Popen(["java", "-jar", "DataCollector.jar",args.ipRabbit], shell=True))
