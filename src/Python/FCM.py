8c23e13e176cc01e3fd02b8d4ceff577ec7cfa9e
Alessandro Sieni <Alexander.sieni@gmail.com>
2019-05-22 17:47:11 +0200

    Aggiunta gestione del merge, della finestra, e dell'unione dei modelli

--- src/Python/FCM.py
+++ src/Python/FCM.py
@@ -4,7 +4,9 @@ import numpy as np
 import json
 from scipy.spatial.distance import cdist
 import os
-
+import time
+import matplotlib.pyplot as plt
+import itertools
 ERROR_THRESHOLD = 0.005
 DISTANCE_THRESHOLD = 100
 VALUES_THRESHOLD = 2
@@ -13,30 +15,84 @@ NEW_VALUES = -5
 MAX_ACCEPTED_OUTLIERS = 1
 BASE_MODEL_PATH = "../../dataNodes/newModel"
 BASE_DATA_PATH = "../../dataNodes/readyData"
-
+MERGED_MODE_NODE_PATH =  "../../dataNodes/NewUpdatedModel.json"
+PREPARED_MODEL = "../../DataSink/preparedModel.json"
+MERGED_MODEL_PATH = "../../DataSink/mergedModel.json"
+BASE_MODEL_SINK_PATH = "../../DataSink/ModelNode"
 
 
 class FCM:
-    def merge(self):
+    def associate(self,distance):
+        if distance <= 0.5:
+            return 1
+        elif 0.5 < distance <= 1 :
+            return 0.75
+        elif 1 < distance <= 2 :
+            return 0.5
+        else:
+            return 0
+    def merge(self,nodes):
+        #Obtaining the centers
+        with open(BASE_MODEL_SINK_PATH+"1.json","r") as model:
+            centers = np.array(json.load(model)["centers"],dtype=float)
+        for i in range(2,nodes+1):
+            #Opening the file and concatenating the centers
+            with open(BASE_MODEL_SINK_PATH+str(i)+".json","r") as model:
+                nodeCntrs = np.array(json.load(model)["centers"],dtype=float)
+                centers = np.vstack((centers,nodeCntrs))
+        print(centers)
+        print(centers.shape)
+
+        cntr,u_orig, _, _, _, _, _ = fuzz.cluster.cmeans(centers.T,2,2,error=ERROR_THRESHOLD,maxiter = MAX_ITER)
+        mergedModel = {}
+        mergedModel["centers"] = cntr.tolist()
+        #Computing the mean Minumum distance for the new centers from the old centers
+        for i in range(1,nodes+1):
+            print("------------- NODO "+str(i)+"--------------------------")
+            with open(BASE_MODEL_SINK_PATH+str(i)+".json","r") as model:
+                oldcntrs = np.array(json.load(model)["centers"])
+            indexes = np.argmin(cdist(cntr,oldcntrs,metric='euclidean'),axis=1)
+            print(cdist(cntr,oldcntrs,metric='euclidean'))
+            #Calculating the minimum value along the row
+            minDistances = np.amin(cdist(cntr,oldcntrs,metric='euclidean'),axis=1)
+            minDistancesIndex =  np.argmin(cdist(cntr,oldcntrs,metric='euclidean'),axis=1)
+            #Check if there are at least a repetition
+            if(np.unique(minDistancesIndex).shape[0] == minDistancesIndex.shape[0]):
+            #Compute the mean of the distances
+                meanDistance = np.mean(minDistances)
+                mergedModel[str(i)] = self.associate(meanDistance)
+            else:
+                mergedModel[str(i)] = 0
+
+        with open(MERGED_MODEL_PATH,"w") as mergedModelFile:
+            json.dump(mergedModel,mergedModelFile)
         return "Models merged",201
-    def train(self, id ,values):
+
+    def train(self, id,coeff,window,values):
         #Retriving the dataframe related to the generated file
         df = pd.read_csv(BASE_DATA_PATH+id+".txt",names=["X","Y"],header=None,dtype={"X":float,"Y":float})
         print("CSV ORIGINALE: "+str(df.shape))
+        dim = int(int(window)*(1+float(coeff)))
         #Checking if the model must be computed
         NEW_VALUES = int(values) * -1
         newValues = df[NEW_VALUES:]
-        #Deleting from the original dataframe the new values
+        #Deleting from the original dataframe the new values and the previous window
+        START_WINDOW = dim * (-1);
         df = df[:NEW_VALUES]
+        print("Dimensione Dataset senza nuovi valori con finestra: "+ str(df.shape))
         print("[DEBUG] Old Dataframe shape :"+str(df.shape))
         [df,result] = self.isModelNeeded(id,df,newValues)
         print("[DEBUG] New Dataframe shape without outliers: "+str(df.shape))
         if(result):
+            #Selecting only the desired window
+            df = df[START_WINDOW:]
+            print("DIMENSION OF THE DATASET ANALYZED: "+ str(df.shape))
             #Training the FCM with the array just obtained
             cntr,u_orig, _, _, _, _, _ = fuzz.cluster.cmeans(np.array(df).T,2,2,error=ERROR_THRESHOLD,maxiter = MAX_ITER)
             #Creating the JSON with the information of the created model
             model = {}
             model["centers"] = cntr.tolist()
+            model["timestamp"] = time.time()
             #model["coefficentsMatrix"] = u_orig.tolist()
             #Saving the JSON in the file
             with open(BASE_MODEL_PATH+id+".json","w") as newModelFile:
@@ -67,3 +123,31 @@ class FCM:
                     return df,False
         else:
             return df,True
+    def update(self,id):
+        with open(BASE_MODEL_PATH+str(id)+".json","r") as oldModelFile:
+            oldModel = np.array(json.load(oldModelFile)['centers'])
+        print(oldModel)
+        with open(MERGED_MODE_NODE_PATH,"r") as mergedModelFile:
+            dict = json.load(mergedModelFile)
+            mergedModel = np.array(dict['centers'])
+            weight = float(dict[str(id)])
+        distances = cdist(mergedModel,oldModel,metric="euclidean")
+        minDistancesIndex = np.argmin(distances,axis=1)
+        print(minDistancesIndex)
+        updatedPoint = []
+        print(mergedModel.shape)
+        for i in range (0,mergedModel.shape[0]):
+            print("Step: " +str(weight))
+            IncrementX = float(mergedModel[i,0])*weight + float(oldModel[minDistancesIndex[i],0])*(1-weight)
+            IncrementY = float(mergedModel[i,1])*weight + float(oldModel[minDistancesIndex[i],1])*(1-weight)
+            updatedPoint.append([IncrementX,IncrementY])
+        a = np.array(updatedPoint)
+
+        #print("Point nearest 2:"+str(mergedModel[1])+"-"+str(oldModel[minDistancesIndex[1]]))
+        plt.scatter(oldModel[:,0],oldModel[:,1], color="red")
+        plt.scatter(mergedModel[:,0],mergedModel[:,1],color="blue")
+        plt.scatter(a[:,0],a[:,1], color="green")
+        plt.savefig("plot"+str(id)+".png")
+        plt.clf()
+        #print(distances)
+        return "Model updated",200
--- src/Python/RestServer.py
+++ src/Python/RestServer.py
@@ -8,11 +8,11 @@ class Server(Resource):
     def post(self):
         print("Gestione Post")
         if(request.json['command'] == "Train"):
-            return FCM().train(request.json['ID'],request.json['values'])
+            return FCM().train(request.json['ID'],request.json["Coeff"],request.json["Window"],request.json['values'])
         elif (request.json['command'] == "Merge"):
-            with open("../../dataSink/MergedModel.json","w") as f:
-                print(request.json['nodes'])
-            return FCM().merge()
+            return FCM().merge(int(request.json['nodes']))
+        elif (request.json["command"] == "Update"):
+            return FCM().update(int(request.json["ID"]))
         else:
             return "Command not available",200
 app = Flask(__name__)
