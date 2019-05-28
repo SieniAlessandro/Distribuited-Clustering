ffef24fa691ecec0be440f66e57c2a463efb3f25
Alessandro Sieni <Alexander.sieni@gmail.com>
2019-05-28 08:35:34 +0200

    Modifica data collector

--- src/Java/src/main/java/it/unipi/cds/federatedLearning/node/DataCollector.java
+++ src/Java/src/main/java/it/unipi/cds/federatedLearning/node/DataCollector.java
@@ -5,6 +5,10 @@ import it.unipi.cds.federatedLearning.Log;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * This class is used to start a simulated connection with simulated sensor nodes
@@ -37,23 +41,34 @@ public class DataCollector {
 		} catch (ArrayIndexOutOfBoundsException e) {
 			Log.error("Sink", "Provide RabbitMQ Server's ip address as argument");
 		}
-		ArrayList<Thread> threads = new ArrayList<>();
+		
+//		ArrayList<Thread> threads = new ArrayList<>();
+//		RepositoryHandler repository = new RepositoryHandler(THRESHOLD, newValuesThreshold);
+//		for(int i = 0; i < numberOfThreads; i++) {
+//			/*
+//			 * the last parameter is used for to specify if there is an infinite number of writes or not
+//			 */
+//			Runnable r = new DataGenerator(repository, numberOfWrites, false);
+//			Thread t = new Thread(r);
+//			t.start();
+//			threads.add(t);
+//		}
+//		/*
+//		 * USED ONLY IF THE NUMBER OF WRITES IS NOT INFINITE
+//		 */
+//		for ( Thread t : threads )
+//			t.join();
 		RepositoryHandler repository = new RepositoryHandler(THRESHOLD, newValuesThreshold);
-		for(int i = 0; i < numberOfThreads; i++) {
-			/*
-			 * the last parameter is used for to specify if there is an infinite number of writes or not
-			 */
-			Runnable r = new DataGenerator(repository, numberOfWrites, false);
-			Thread t = new Thread(r);
-			t.start();
-			threads.add(t);
+		ExecutorService threadPool = Executors.newFixedThreadPool(numberOfThreads);
+		for (int i = 0; i < numberOfThreads;i++) {
+			// We use execute instead of submit beacuese we are not interested in the future variable related to the thread
+			// since the Class DataGenerator is an implementation of runnable instead callable and we don't check when the single
+			// threads are terminated
+			threadPool.execute( new DataGenerator(repository, numberOfWrites, false));
 		}
-		/*
-		 * USED ONLY IF THE NUMBER OF WRITES IS NOT INFINITE
-		 */
-		for ( Thread t : threads )
-			t.join();
-
+		//Waiting the termination of all threads
+		threadPool.shutdown();
+		threadPool.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS);
 		try {
 			DataCollector.nodeCommunicationHandler.callFunction("Leave");
 		} catch (IOException | InterruptedException e) {
